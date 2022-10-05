7776126916f76569f6503f89d278ba78bd0d51d3
Kevin Dages <kevin.dages79@gmail.com>
2022-10-05 11:40:43 -0700

    This is the final version of TDD for EFCore

---
+++ EFCoreTutorial.Tests/SongRepositoryTests.cs
@@ -0,0 +1,89 @@
+﻿using EFCoreTutorial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreTutorial.Tests
+{
+    public class SongRepositoryTests : IDisposable
+    {
+        private MusicContext db;
+        private SongRepository underTest;
+
+        public SongRepositoryTests()
+        {
+            db = new MusicContext();
+
+            db.Database.BeginTransaction();
+
+            underTest = new SongRepository(db);
+        }
+        [Fact]
+        public void Count_Starts_At_Zero()
+        {
+           // var db = new MusicContext();
+           // var underTest = new SongRepository(db);
+
+            var count = underTest.Count();
+
+            Assert.Equal(0, count);
+        }
+        [Fact]
+        public void Create_Increases_Count()
+        {
+            //var db = new MusicContext();
+           // var underTest = new SongRepository(db);
+
+            underTest.Create(new Song() { Title = "Foo" });
+
+            var count = underTest.Count();
+            Assert.Equal(1, count);
+        }
+
+
+        public void Dispose()
+        {
+            db.Database.RollbackTransaction();
+        }
+
+        //Consolidated all test into this file
+        [Fact]
+        public void GetById_Returns_Created_Item()
+        {
+            var expectedSong = new Song() { Title = "Baby Shark" };
+            underTest.Create(expectedSong);
+
+            var result = underTest.GetById(expectedSong.Id);  // The Id was set by EF when we call Create above.
+
+            Assert.Equal(expectedSong.Title, result.Title);
+        }
+
+        [Fact]
+        public void Delete_Reduces_Count()
+        {
+            var song = new Song() { Title = "Baby Shark" };
+            underTest.Create(song);
+
+            underTest.Delete(song);
+            var count = underTest.Count();
+
+            Assert.Equal(0, count);
+        }
+
+        [Fact]
+        public void GetAll_Returns_All()
+        {
+            underTest.Create(new Song() { Title = "Baby Shark" });
+            underTest.Create(new Song() { Title = "Never gonna give you up" });
+
+            var all = underTest.GetAll();
+
+            Assert.Equal(2, all.Count());
+        }
+
+        // Save or Update?
+    }
+}
+
---
+++ EFCoreTutorial/Models/SongRepository.cs
@@ -0,0 +1,19 @@
+﻿using EFCoreTutorial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreTutorial
+{
+
+        public class SongRepository : Repository<Song>
+        {
+            public SongRepository(MusicContext context) : base(context)
+            {
+                // This ensures our base constructor gets run.
+            }
+        }
+    }
+
